84402ecb32a3323fb0caee7a398354a4ce66fcf2
walkor <worker-man@qq.com>
2015-02-03 21:04:40 +0800

    opt

--- Applications/Statistics/start.php
+++ Applications/Statistics/start.php
@@ -24,3 +24,24 @@ $statistic_worker->name = 'StatisticWorker';
 $web = new WebServer("http://0.0.0.0:55757");
 $web->name = 'StatisticWeb';
 $web->addRoot('www.your_domain.com', __DIR__.'/Web');
+
+// recv udp broadcast
+$udp_finder = new Worker("Text://0.0.0.0:55858");
+$udp_finder->transport = 'udp';
+$udp_finder->onMessage = function ($connection, $data)
+{
+    $data = json_decode($data, true);
+    if(empty($data))
+    {
+        return false;
+    }
+
+    // 无法解析的包
+    if(empty($data['cmd']) || $data['cmd'] != 'REPORT_IP' )
+    {
+        return false;
+    }
+
+    // response
+    return $connection->send(json_encode(array('result'=>'ok')));
+};
