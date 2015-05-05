f21fc5301e4013abce8af0edc4a54d27404e6fb8
walkor <worker-man@qq.com>
2015-05-06 00:07:44 +0800

    mit

--- Applications/Statistics/Web/_init.php
+++ Applications/Statistics/Web/_init.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * This file is part of workerman.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the MIT-LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author walkor<walkor@workerman.net>
+ * @copyright walkor<walkor@workerman.net>
+ * @link http://www.workerman.net/
+ * @license http://www.opensource.org/licenses/mit-license.php MIT License
+ */
 define('ST_ROOT', realpath(__DIR__.'/../'));
 require_once ST_ROOT .'/Lib/functions.php';
 require_once ST_ROOT .'/Lib/Cache.php';
