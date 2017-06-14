16ffe0f95b26b6d7c4bf682677afe08ad134ae5b
walkor <walkor@users.noreply.github.com>
2017-06-14 16:03:58 +0800

    Create functions.php

--- Applications/Statistics/Lib/functions.php
+++ Applications/Statistics/Lib/functions.php
@@ -133,7 +133,7 @@ function check_auth()
  */
 function _session_start()
 {
-    if(class_exists('\Workerman\Worker'))
+    if(PHP_SAPI === 'cli')
     {
         return Http::sessionStart();
     }
@@ -146,7 +146,7 @@ function _session_start()
  */
 function _exit($str = '')
 {
-    if(class_exists('\Workerman\Worker'))
+    if(PHP_SAPI === 'cli')
     {
         return Http::end($str);
     }
